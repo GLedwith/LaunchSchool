fa1e1b0aa0294effe867bcbb78b11a55946cd249
Garry Ledwith <garryledwith@gmail.com>
2019-12-06 10:33:38 +0000

    Exercises complete

--- RB101/lesson_2/calculator.rb
+++ RB101/lesson_2/calculator.rb
@@ -1,30 +1,104 @@
-# ask the user for two numbers
-# ask the user for an operation to perform
-# performs the operation on the two numbers
-# output the result
 
-# answer = Kernel.gets()
-# Kernel.puts(answer)
+require 'pry'
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
 
-Kernel.puts("Welcome to calculator!")
-
-Kernel.puts("What's your first number?")
-number1 = Kernel.gets().chomp()
-
-Kernel.puts("What's your second number?")
-number2 = Kernel.gets().chomp()
-
-Kernel.puts("What opertaion would you like to perofm? 1) add, 2) subtract, 3) multiply, 4) divide")
-operation = Kernel.gets().chomp()
-
-if operation == '1'
-  result =  number1.to_i() + number2.to_i()
-elsif operation == '2'
-  result =  number1.to_i() - number2.to_i()
-elsif operation == '3'
-  result =  number1.to_i() * number2.to_i()
-elsif operation == '4'
-  result =  number1.to_f() / number2.to_f()
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-Kernel.puts("The result is #{result}")
+def valid_number?(input)
+  input.to_i.to_s == input
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+prompt(MESSAGES['welcome'])
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt(MESSAGES['valid_name'])
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}!")
+
+loop do # Main loop
+  number1 = ''
+  loop do
+    prompt(MESSAGES['number1'])
+    number1 = Kernel.gets().chomp()
+
+    break if valid_number?(number1)
+    prompt(MESSAGES['invalid_entery'])
+  end
+
+  number2 = ''
+  loop do
+    prompt(MESSAGES['number2'])
+    number2 = Kernel.gets().chomp()
+
+    break if valid_number?(number2)
+    prompt(MESSAGES['invalid_entery'])
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt(MESSAGES['invalid_operator'])
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number1.to_f() / number2.to_f()
+           end
+
+  prompt("The result is #{result}!")
+  binding.pry 
+
+  prompt(MESSAGES['another_calc'])
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt(MESSAGES['thank_you'])
