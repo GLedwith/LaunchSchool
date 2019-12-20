12763771d5419050d5469771ab10a0e0d50da58f
Garry Ledwith <garryledwith@gmail.com>
2019-12-20 08:27:14 +0000

    Refactored after code review

--- RB101/lesson_2/car_loan_calculator.rb
+++ RB101/lesson_2/car_loan_calculator.rb
@@ -1,8 +1,3 @@
-# Car loan calculator:
-# p = loan amount
-# j = monthly interest rate
-# n = loan duration in months
-
 require 'yaml'
 MESSAGES = YAML.load_file('car_loan_calculator_config.yml')
 
@@ -24,71 +19,77 @@ def valid_years?(input)
   input.to_i.to_s == input && input.to_i > 0
 end
 
-def valid_another_calculation_answer?(choice)
-  %w(y n).include?(choice)
+def retrieve_loan_amount
+  prompt(MESSAGES['loan_amount'])
+  loop do
+    amount = gets.chomp
+    return amount if valid_number?(amount)
+    prompt(MESSAGES['invalid_input'])
+  end
 end
 
-def another_calculation?(choice)
-  choice == 'y' || choice == 'yes'
+def retrieve_interest_rate
+  prompt(MESSAGES['interest_rate'])
+  prompt(MESSAGES['interest_rate_example'])
+  loop do
+    interest_rate = gets.chomp
+    return interest_rate if valid_number?(interest_rate)
+    prompt(MESSAGES['invalid_input'])
+  end
 end
 
-def payment_months(p, j, n)
-  p.to_f * (j / (1 - (1 + j)**(-n)))
+def retrieve_loan_duration_years
+  prompt(MESSAGES['loan_years'])
+  loop do
+    loan_duration_years = gets.chomp
+    return loan_duration_years if valid_years?(loan_duration_years)
+    prompt(MESSAGES['invalid_entry'])
+  end
+end
+
+def another_calculation?
+  prompt(MESSAGES['another_calc'])
+  answer = ''
+  loop do
+    answer = gets.chomp.downcase
+    break if answer == 'y' || answer == 'n'
+    prompt(MESSAGES['invalid'])
+  end
+  answer == 'y'
+end
+
+def monthly_payment(amount, monthly_int, loan_in_months)
+  amount.to_i * (monthly_int / (1 - (1 + monthly_int)**(-loan_in_months)))
+end
+
+def retrieve_name
+  prompt(MESSAGES['first_name'])
+  loop do
+    first_name = gets.chomp
+    return first_name unless first_name.empty? || first_name.strip!
+    prompt(MESSAGES['valid_name'])
+  end
 end
 
 prompt(MESSAGES['welcome'])
 
-first_name = nil
-
-loop do
-  prompt(MESSAGES['first_name'])
-  first_name = gets.chomp
-
-  first_name.empty? ? prompt(MESSAGES['valid_name']) : break
-end
-
-prompt("Hi #{first_name}!")
+prompt("Hi #{retrieve_name}!")
 
 loop do #------------------Main loop---------------------
-  p = nil
-  loop do
-    prompt(MESSAGES['loan_amount'])
-    p = gets.chomp
+  amount = retrieve_loan_amount
+  interest_rate = retrieve_interest_rate
+  loan_duration_years = retrieve_loan_duration_years
 
-    break if valid_number?(p)
-    prompt(MESSAGES['invalid_input'])
-  end
+  anual_interest_rate = interest_rate.to_f / 100
+  monthly_int = anual_interest_rate.to_f / MONTHS_IN_YEAR
+  loan_in_months = loan_duration_years.to_f * MONTHS_IN_YEAR
 
-  anual_interest_rate = nil
-
-  loop do
-    prompt(MESSAGES['interest_rate'])
-    prompt(MESSAGES['interest_rate_example'])
-    anual_interest_rate = gets.chomp
-
-    break if valid_number?(anual_interest_rate)
-    prompt(MESSAGES['invalid_input'])
-  end
-
-  loan_duration_years = nil
-
-  loop do
-    prompt(MESSAGES['loan_years'])
-    loan_duration_years = gets.chomp
-
-    break if valid_years?(loan_duration_years)
-    prompt(MESSAGES['invalid_entry'])
-  end
-
-  j = anual_interest_rate.to_f / MONTHS_IN_YEAR # j = monthly interest rate
-  n = loan_duration_years.to_i * MONTHS_IN_YEAR # n = loan duration in months
   clear
-  prompt("Your monthly payment is $#{format('%0.2f', payment_months(p, j, n))}")
+  puts "Your monthly payment is:"
+  prompt"$#{monthly_payment(amount, monthly_int, loan_in_months).round(2)}"
 
-  prompt(MESSAGES['another_calc'])
-  choice = gets.chomp.downcase
+  break unless another_calculation?
   clear
-  break unless another_calculation?(choice)
 end
 
 prompt(MESSAGES['thank_you'])
